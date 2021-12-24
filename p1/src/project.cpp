a14a75a5d482d33b6daca4ca691d8bb4c0ebe03d
A-Torrres <andre.torres@tecnico.ulisboa.pt>
2021-12-24 00:18:19 +0000

    Referencias de sequencias e espaçamentos

--- p1/src/project.cpp
+++ p1/src/project.cpp
@@ -3,17 +3,15 @@
 #include <sstream>
 #include <string>
 
-
 #define ull unsigned long long
 
 using namespace std;
 
 
-void readSequenceToVector(vector<int>* vec) {
+void readSequenceToVector(vector<int>& vec) {
     int number;
     string line;
 
-    
     getline(cin >> ws, line);
     /*
     if(line.empty()) {
@@ -23,45 +21,43 @@ void readSequenceToVector(vector<int>* vec) {
 
     istringstream iss(line);
     while(iss >> number) {   
-        vec->push_back(number); 
+        vec.push_back(number); 
     }
 }
 
 
-pair<ull, ull> findNumberAndLengthOfLIS(vector<int>* sequence) {
-
+pair<ull, ull> findNumberAndLengthOfLIS(vector<int>& sequence) {
     ull maxLength = 0;
     ull subsequenceCount = 0;
-    ull sequenceLength = sequence->size();
+    ull sequenceLength = sequence.size();
 
     // Initialize all the lengths with ones;
     vector<ull> lengths(sequenceLength, 1);
     // Initialize all the counts with ones
     vector<ull> counts(sequenceLength, 1);
 
-    
     for(ull i = 0; i < sequenceLength; i++) {
         for(ull j = 0; j < i; j++) {
-
-            if((*sequence)[i] <= (*sequence)[j]) {
+            if(sequence[i] <= sequence[j]) {
                 continue;
             }
 
             if(lengths[j] + 1 > lengths[i]) {
                 lengths[i] = lengths[j] + 1;
                 counts[i] = counts[j];
-            } else if (lengths[j] + 1 == lengths[i]) {
+            } 
+            else if (lengths[j] + 1 == lengths[i]) {
                 counts[i] += counts[j];
             }
         }
         
         if (maxLength == lengths[i]) {
             subsequenceCount += counts[i];
-        } else if (lengths[i] > maxLength) {
+        } 
+        else if (lengths[i] > maxLength) {
             maxLength = lengths[i];
             subsequenceCount = counts[i];
         }
-               
     }
 
     //Forma menos eficiente de calcular o maxLength e subsequencesCount
@@ -81,19 +77,19 @@ pair<ull, ull> findNumberAndLengthOfLIS(vector<int>* sequence) {
 }
 
 
-ull findLengthOfLCIS(vector<int>* seq1, vector<int>* seq2) {
-    vector<ull> lengths(seq2->size(), 0);
+ull findLengthOfLCIS(vector<int>& seq1, vector<int>& seq2) {
+    vector<ull> lengths(seq2.size(), 0);
     ull currentLength;
     ull maxLength = 0;
 
-    for(ull i = 0; i < seq1->size(); i++) {
+    for(ull i = 0; i < seq1.size(); i++) {
         currentLength = 0;
 
-        for(ull j = 0; j < seq2->size(); j++) {
-            if((*seq1)[i] > (*seq2)[j] && lengths[j] > currentLength) {
+        for(ull j = 0; j < seq2.size(); j++) {
+            if(seq1[i] > seq2[j] && lengths[j] > currentLength) {
                 currentLength = lengths[j];
             }
-            else if((*seq1)[i] == (*seq2)[j]) {
+            else if(seq1[i] == seq2[j]) {
                 lengths[j] = currentLength + 1;
 
                 // Update the maximum value of lengths
@@ -112,24 +108,21 @@ void handleFirstProblem() {
     vector<int> sequence;
     pair<ull, ull> result;
 
-    readSequenceToVector(&sequence);
-
-    result = findNumberAndLengthOfLIS(&sequence);
+    readSequenceToVector(sequence);
+    result = findNumberAndLengthOfLIS(sequence);
 
     cout << result.first << " " << result.second << endl;
-
 }
-
 
 
 void handleSecondProblem() {
     vector<int> sequence1;
     vector<int> sequence2;
 
-    readSequenceToVector(&sequence1);
-    readSequenceToVector(&sequence2);
+    readSequenceToVector(sequence1);
+    readSequenceToVector(sequence2);
 
-    cout << findLengthOfLCIS(&sequence1, &sequence2) << endl;
+    cout << findLengthOfLCIS(sequence1, sequence2) << endl;
 }
 
 
