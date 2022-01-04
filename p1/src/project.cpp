fccf9947fece6b707228f93bef9104abb5aad17b
A-Torrres <andre.torres@tecnico.ulisboa.pt>
2022-01-04 22:11:35 +0000

    Comments and some minor changes

--- p1/src/project.cpp
+++ p1/src/project.cpp
@@ -1,3 +1,4 @@
+// Proj1 ASA 2021-2022 tp19 ist199053 ist199074
 #include <iostream>
 #include <vector>
 #include <sstream>
@@ -6,7 +7,7 @@
 #include <unordered_map>
 #include <algorithm>
 
-#define ull unsigned long long
+#define ll long long
 
 using namespace std;
 
@@ -26,36 +27,36 @@ void readSequenceToVector(vector<int>& vec) {
 
 // Time Complexity: O(N)
 void removeConsecutiveDuplicates(vector<int>& vet, unordered_map<int, int>& m) {
-	int last = INT_MAX;
-    ull k = 0;
+    int last = INT_MAX;
+    ll k = 0;
 
-	for (ull i = 0; i < vet.size(); i++) {
-		// if m.find(x) == m.end() then it means that
-		// x is not present in the map
-		if(m.find(vet[i]) == m.end()) {
-			m[vet[i]] = 1; // inserting vet[i] into the map
-		}
-        else if(vet[i] == last){
+	for (ll i = 0; i < vet.size(); i++) {
+
+		if(m.find(vet[i]) == m.end())   // if m.find(x) == m.end() then x is not in the map yet
+			m[vet[i]] = 1;  // inserting vet[i] into the map
+
+        else if(vet[i] == last) // if vet[i] == last it's a consecutive duplicate
             continue;
-        }
-        vet[k++] = vet[i];
-        last = vet[i];
-	}
-    //cout << "removeDuplicates: " << vet.size() <<' '<< k <<' '<< vet.size()-k << endl;
-    vet.resize(k);
+        
+        vet[k++] = vet[i];  // add element and increase number of elements
+        last = vet[i];  // update last element seen
+    }
+    
+    vet.resize(k);  // resize vet with number of elements not duplicated
 }
 
 
 // Time Complexity: O(N), where N equals max(v1.size(), v2.size())
 void removeUncommonElements(vector<int>& v1, vector<int>& v2, unordered_map<int, int>& m1, unordered_map<int, int>& m2) {
-    ull k = 0;
-    for(ull i = 0; i < v1.size(); i++)
-        if(m2.find(v1[i]) != m2.end())
-            v1[k++] = v1[i];
-    v1.resize(k);
+    ll k = 0;
+    
+    for(ll i = 0; i < v1.size(); i++)   // for every element in v1
+        if(m2.find(v1[i]) != m2.end())  // check if element is in m2
+            v1[k++] = v1[i];    // add element and increase number of elements
+    v1.resize(k);   // resize v1 with number of v1 elements in m2
 
     k = 0;
-    for(ull i = 0; i < v2.size(); i++)
+    for(ll i = 0; i < v2.size(); i++)
         if(m1.find(v2[i]) != m1.end())
             v2[k++] = v2[i];
     v2.resize(k);
@@ -68,7 +69,7 @@ void removeUncommonElements(vector<int>& v1, vector<int>& v2, unordered_map<int,
 void preprocessSequences(vector<int>& s1, vector<int>& s2) {
 	unordered_map<int, int> m1, m2;
 
-    // remove duplicates and builds the unordered_map
+    // builds the unordered_map while removing consecutive duplicates
     removeConsecutiveDuplicates(s1, m1);   
     removeConsecutiveDuplicates(s2, m2);
     
@@ -76,153 +77,78 @@ void preprocessSequences(vector<int>& s1, vector<int>& s2) {
 }
 
 
-// O(nlogn)
-ull findLengthOfLIS(vector<int>& vet, ull size) {
-    if(size == 0) return 0;
+pair<ll, ll> findNumberAndLengthOfLIS(int* sequence, ll size) {
+    ll maxLength = 0, subsequenceCount = 0;
     
-    vector<int> lis;
-    lis.push_back(vet[0]);
-
-    for(ull i = 1; i < size; i++)
-        if(lis.back() < vet[i])
-            lis.push_back(vet[i]);
-        else {
-            ull aux = lower_bound(lis.begin(), lis.end(), vet[i]) - lis.begin();
-            lis[aux] = vet[i];
-        }
-
-    return lis.size();
-}
-
-
-pair<ull, ull> findNumberAndLengthOfLIS(int* sequence, ull size) {
-    ull maxLength = 0,
-        subsequenceCount = 0;
-    
-    vector<ull> lengths(size, 1);
-    vector<ull> counts(size, 1);
-    
+    vector<ll> lengths(size, 1);
+    vector<ll> counts(size, 1);
     vector<int> sorted(size);
+
     copy(sequence, sequence + size, begin(sorted));
     sort(begin(sorted), end(sorted));
 
-    for(ull i = 0; i < size; i++) {
-        for(long j = i - 1; j >= 0; j--) {
+    for(ll i = 0; i < size; i++) {
+        ll j = i - 1;
 
-            if(sequence[j] >= sequence[i]) {
+        // ignore all the first previous elements bigger or equal to sequence[i]
+        for(; j >= 0; j--) {
+            if(sequence[j] < sequence[i])
                 continue;
-            }
+        }
 
-            ull idx = lower_bound(sorted.begin(), sorted.end(), sequence[i]) - sorted.begin() - 1;
-            
-            for(long k = j; k >= 0; k--) {
-                if(lengths[k] >= lengths[i] && sequence[k] < sequence[i]) {
-                    lengths[i] = lengths[k] + 1;
-                    counts[i] = counts[k];
-                    j = k;
-                    if(sequence[k] == sorted[idx])
-                        break;
-                }
-            }
-            for(j--; j >= 0; j--) {
-                if(lengths[j] == lengths[i] - 1 && sequence[j] < sequence[i]) {
-                    counts[i] += counts[j];
-                }
-            }
-        }
-        if (maxLength < lengths[i]) {
-            maxLength = lengths[i];
-            subsequenceCount = counts[i];
-        }
-        else if (maxLength == lengths[i]) {
-            subsequenceCount += counts[i];
-        }
-    }
-    /*
-    for(ull i = 0; i < size; i++)
-        cout << sequence[i] << ' ';
-    cout << endl;
-    for(ull i = 0; i < size; i++)
-        cout << lengths[i] << ' ';
-    cout << endl;
-    for(ull i = 0; i < size; i++)
-        cout << counts[i] << ' ';
-    cout << endl;
-    */
-    return pair<ull, ull>(maxLength, subsequenceCount);
-}
-
-pair<ull, ull> findNumberAndLengthOfLIS1(int* sequence, ull size) {
-    ull maxLength = 0,
-        subsequenceCount = 0;
-    
-    /*ull lengths[100000];
-    ull counts[100000];
-    ull* lengths = new ull[size];
-    ull* counts = new ull[size];*/
-    vector<ull> lengths(size, 1);
-    vector<ull> counts(size, 1);
-    /*// Initialize with ones;
-    for(ull i = 0; i < size; i++) {
-        lengths[i] = 1; 
-        counts[i] = 1;
-    }*/
-    for(ull i = 0; i < size; i++) {
-        for(ull j = 0; j < i; j++) {
-            
-            if(sequence[j] >= sequence[i]) {
-                continue;
-            }
-            if(lengths[j] + 1 > lengths[i]) {
-                lengths[i] = lengths[j] + 1;
-                counts[i] = counts[j];
-            } 
-            else if (lengths[j] + 1 == lengths[i]) {
-                counts[i] += counts[j];
-            }
-        }
+        // find the previous element of sequence[i] in sorted sequence
+        ll current_idx = lower_bound(sorted.begin(), sorted.end(), sequence[i]) - sorted.begin();
+        ll previous = sorted[current_idx - 1];
         
+        // update current length, length[i]
+        for(ll k = j; k >= 0; k--) {
+            if(lengths[k] >= lengths[i] && sequence[k] < sequence[i]) {
+                lengths[i] = lengths[k] + 1;
+                counts[i] = counts[k];
+                j = k;  // update position
+                // if current == previous we don't need to continue searching
+                if(sequence[k] == previous)
+                    break;
+            }
+        }
+
+        // find how many paths we can take to reach the current length
+        for(j--; j >= 0; j--) {
+            if(lengths[j] == lengths[i] - 1 && sequence[j] < sequence[i])
+                counts[i] += counts[j];
+        }
+
+        // update maxLength and subsequenceCount
         if (maxLength < lengths[i]) {
             maxLength = lengths[i];
             subsequenceCount = counts[i];
         }
-        else if (maxLength == lengths[i]) {
+        else if (maxLength == lengths[i])
             subsequenceCount += counts[i];
-        } 
-    }/*
-    for(ull i = 0; i < size; i++)
-        cout << sequence[i] << ' ';
-    cout << endl;
-    for(ull i = 0; i < size; i++)
-        cout << lengths[i] << ' ';
-    cout << endl;
-    for(ull i = 0; i < size; i++)
-        cout << counts[i] << ' ';
-    cout << endl;*/
-    return pair<ull, ull>(maxLength, subsequenceCount);
+    }
+
+    return pair<ll, ll>(maxLength, subsequenceCount);
 }
 
 
-ull findLengthOfLCIS(vector<int>& seq1, vector<int>& seq2) {
-    vector<ull> lengths(seq2.size(), 0);
-    ull currentLength, 
-        maxLength = 0;
+ll findLengthOfLCIS(vector<int>& seq1, vector<int>& seq2) {
+    vector<ll> lengths(seq2.size(), 0);
+    ll currentLength, maxLength = 0;
     
-    for(ull i = 0; i < seq1.size(); i++) {
+    for(ll i = 0; i < seq1.size(); i++) {
         currentLength = 0;
 
-        for(ull j = 0; j < seq2.size(); j++) {
-            if(seq1[i] > seq2[j] && lengths[j] > currentLength) {
+        for(ll j = 0; j < seq2.size(); j++) {
+            if(seq1[i] > seq2[j] && lengths[j] > currentLength)
                 currentLength = lengths[j];
-            }
+            
             else if(seq1[i] == seq2[j]) {
                 lengths[j] = currentLength + 1;
 
-                // Update the maximum value of lengths
-                if(lengths[j] > maxLength) {
+                // update maxLength
+                if(lengths[j] > maxLength)
                     maxLength = lengths[j];
-                }
-            } 
+            }
         }
     }
 
@@ -232,8 +158,8 @@ ull findLengthOfLCIS(vector<int>& seq1, vector<int>& seq2) {
 
 void handleFirstProblem() {
     vector<int> sequence;
-    pair<ull, ull> result;
-    pair<ull, ull> result1;
+    pair<ll, ll> result;
+    pair<ll, ll> result1;
 
     readSequenceToVector(sequence);
     result = findNumberAndLengthOfLIS(&(sequence[0]), sequence.size());
@@ -258,11 +184,10 @@ int main() {
 
     cin >> problem;
 
-    if(problem == 1) {
+    if(problem == 1) 
         handleFirstProblem();
-    } 
-    else if (problem == 2) {
+    else if (problem == 2)
         handleSecondProblem();
-    }
+    
     return 0;
 }
