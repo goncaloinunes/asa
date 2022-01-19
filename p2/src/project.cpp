38e9550dbf347ded61fb2b78c62603c819f2a6e2
Gonçalo Nunes <goncaloinunes2002@gmail.com>
2022-01-19 00:19:19 +0000

    Finished input reading

--- p2/src/project.cpp
+++ p2/src/project.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
+typedef vector<vector<unsigned int>> graph;
+
+
+graph* build_graph(unsigned int numVertices) {
+    unsigned int x, y;
+    graph graph(numVertices+1);
+    unordered_map<unsigned int, unsigned int> map;
+    
+
+    for(size_t i = 0; i < numVertices; i++) {
+        if(scanf("%u %u", &x, &y) != 2) {
+            puts("0");
+            exit(EXIT_SUCCESS);
+        }
+
+        graph[x].push_back(y);
+
+        // Se o vertice tem mais de dois pais, saimos
+        if(++map[y] > 2) {
+            puts("0");
+            exit(EXIT_SUCCESS);
+        }
+    }
+
+    return &graph;
+}
+
 
 int main() {
-    unsigned int x, y, v1, v2, numVertices, numEdges;
+    
+    unsigned int v1, v2, numVertices, numEdges;
 
     scanf("%u %u", &v1, &v2);
     scanf("%u %u", &numVertices, &numEdges);
@@ -12,11 +42,10 @@ int main() {
     // Condição suficiente mas não necessária para o grafo ser um DAG
     if(numVertices < numEdges - 1) {
         puts("0");
+        return 0;
     }
 
-    while(scanf("%u %u", &x, &y)) {
-        //adicionar x e y a uma estrutura de dados
-    }
+    graph *graph = build_graph(numVertices);
 
     
     return 0;
