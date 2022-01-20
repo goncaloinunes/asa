310a3bf8f15ecda184e8e6f65bb1dde1ec25bdb5
Gonçalo Nunes <goncaloinunes2002@gmail.com>
2022-01-20 00:27:05 +0000

    Added function to check if graph is a DAG

--- p2/src/project.cpp
+++ p2/src/project.cpp
@@ -4,31 +4,85 @@
 
 using namespace std;
 
-typedef vector<vector<unsigned int>> graph;
+enum COLOR {
+    WHITE,
+    GREY,
+    BLACK
+};
 
-
-graph* build_graph(unsigned int numVertices) {
-    unsigned int x, y;
-    graph graph(numVertices+1);
-    unordered_map<unsigned int, unsigned int> map;
+class Graph {
+    public:
+        vector<vector<unsigned int>> adj;
+        vector<COLOR> colors;
+        unsigned int numVertices;
+        unsigned int numEdges;
     
+        Graph(unsigned int vertices, unsigned int edges) {
+            numVertices = vertices;
+            numEdges = edges;
+            adj.resize(numVertices+1);
+            colors.assign(numVertices+1, WHITE);
+        }
 
-    for(size_t i = 0; i < numVertices; i++) {
+        void addEdge(unsigned int v1, unsigned int v2) {
+            adj[v1].push_back(v2);
+
+            // Se o vertice tem mais de dois pais, saimos
+            if(adj[v1].size() > 2) {
+                puts("0");
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+        bool IsDAG() {
+            // Encontrar o vertice com out-degree 0
+            unsigned int vertice = 0;
+            for (size_t i = 1; i < adj.size(); i++) {
+                if(adj[i].size() == 0) {
+                    vertice = i;
+                    break;
+                }
+            }
+
+            // Se esse vertice não existir, então o grafo não é um DAG
+            if(vertice == 0) {
+                return false;
+            }
+
+            // Fazer DFS  apartir desse vertice 
+            return Dfs(vertice);
+        }
+
+
+        bool Dfs(unsigned int v) {
+            colors[v] = WHITE;
+            for (unsigned int u : adj[v]) {
+                if (colors[u] == WHITE) {
+                    if(!Dfs(u)) {
+                        return false;
+                    }
+                } else if (colors[u] == GREY) {
+                    return false;
+                }
+            }
+            colors[v] = BLACK;
+            return true;
+
+        }   
+};
+
+
+void build_inverted_graph(Graph* graph) {
+    unsigned int x, y;
+    
+    for(size_t i = 0; i < graph->numVertices; i++) {
         if(scanf("%u %u", &x, &y) != 2) {
             puts("0");
             exit(EXIT_SUCCESS);
         }
 
-        graph[x].push_back(y);
-
-        // Se o vertice tem mais de dois pais, saimos
-        if(++map[y] > 2) {
-            puts("0");
-            exit(EXIT_SUCCESS);
-        }
+        graph->addEdge(y, x);
     }
-
-    return &graph;
 }
 
 
@@ -45,7 +99,8 @@ int main() {
         return 0;
     }
 
-    graph *graph = build_graph(numVertices);
+    Graph* graph = new Graph(numVertices, numEdges);
+    build_inverted_graph(graph);
 
     
     return 0;
