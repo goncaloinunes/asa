a6a7652777968fdd2507b43a7384a67d629d56c1
Gonçalo Nunes <goncaloinunes2002@gmail.com>
2022-01-21 23:22:24 +0000

    Added LCAs function

--- p2/src/project.cpp
+++ p2/src/project.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <list>
+#include <algorithm>
 
 using namespace std;
 
-enum COLOR {
-    WHITE,
-    GREY,
-    BLACK
-};
+#define WHITE  0
+#define GREY  1
+#define BLACK  2
+#define GREY_SECOND_DFS 3
+#define COMMON_ANCESTOR  4
 
 class Graph {
     public:
         vector<vector<unsigned int>> adj;
-        vector<COLOR> colors;
-        unsigned int numVertices;
-        unsigned int numEdges;
+        vector<char> colors;
+        vector<unsigned int> original_out_degrees;
+        unsigned int number_vertices;
+        unsigned int number_edges;
     
         Graph(unsigned int vertices, unsigned int edges) {
-            numVertices = vertices;
-            numEdges = edges;
-            adj.resize(numVertices+1);
-            colors.assign(numVertices+1, WHITE);
+            number_vertices = vertices;
+            number_edges = edges;
+            adj.resize(number_vertices+1);
+            colors.assign(number_vertices+1, WHITE);
+            original_out_degrees.assign(number_vertices+1, 0);
         }
 
         void addEdge(unsigned int v1, unsigned int v2) {
@@ -32,28 +35,33 @@ class Graph {
                 puts("0");
                 exit(EXIT_SUCCESS);
             }
+
+            original_out_degrees[v2]++;
         }
 
         bool IsDAG() {
-            // Condição suficiente mas não necessária para o grafo ser um DAG
-            if(numVertices < numEdges - 1) {
+            // Condição suficiente mas não necessária para o grafo não ser um DAG
+            if(number_vertices < number_edges - 1) {
                 return false;
             }
 
-            for (size_t v = 1; v < numVertices+1; v++) {
-                if (colors[v] == WHITE && Dfs(v))
+            for (size_t v = 1; v < number_vertices+1; v++) {
+                if (colors[v] == WHITE && IsDagVisit(v)) {
+                    colors.assign(number_vertices+1, WHITE);
                     return false;
+                }
             }
 
+            colors.assign(number_vertices+1, WHITE);
             return true;
         }
 
 
-        bool Dfs(unsigned int v) {
+        bool IsDagVisit(unsigned int v) {
             colors[v] = GREY;
-            for (unsigned int u : adj[v]) {
+            for(unsigned int u : adj[v]) {
                 if (colors[u] == WHITE) {
-                    if(Dfs(u)) {
+                    if(IsDagVisit(u)) {
                         return true;
                     }
                 } else if (colors[u] == GREY) {
@@ -61,16 +69,85 @@ class Graph {
                 }
             }
             colors[v] = BLACK;
+            
             return false;
 
-        }   
+        }
+
+        void FirstDfsVisit(unsigned int v) {
+            colors[v] = GREY;
+            for(unsigned int u : adj[v]) {
+                if(colors[u] == WHITE) {
+                    FirstDfsVisit(u);
+                }
+            }
+            colors[v] = BLACK;
+        }
+
+        void unmarkParents(unsigned int v) {
+            for(unsigned int u : adj[v]) {
+                if(colors[u] == BLACK) {
+                    FirstDfsVisit(u);
+                }
+            }
+            colors[v] = GREY;
+        }
+
+        void SecondDfsVisit(unsigned int v, list<unsigned int>* lcas) {
+            colors[v] = GREY_SECOND_DFS;
+            for(unsigned int u : adj[v]) {
+                if(colors[u] == WHITE) {
+                    SecondDfsVisit(u, lcas);
+                } else if (colors[u] == BLACK) {
+                    for(unsigned int parent : adj[v]) {
+                        lcas->remove(parent);
+                    }
+
+                    unmarkParents(u);
+                    lcas->push_front(u);
+                    colors[u] = COMMON_ANCESTOR;
+                    continue;
+                }
+            }
+            colors[v] = BLACK;
+            
+        }
+
+        void LCAs(unsigned int v1, unsigned int v2) {
+            list<unsigned int> lcas;
+
+            FirstDfsVisit(v1);
+
+            if(colors[v2] == BLACK) {
+                lcas.push_front(v2);
+                colors[v2] = COMMON_ANCESTOR;
+            } else {
+                SecondDfsVisit(v2, &lcas);
+            }
+
+            
+            lcas.sort();
+
+            if(lcas.size() > 0) {
+                for(unsigned int v : lcas) {
+                    cout << v << " ";
+                }
+                cout << endl;
+                return;
+            }
+
+            puts("-");
+
+        }
+
+
 };
 
 
 void build_inverted_graph(Graph* graph) {
     unsigned int x, y;
     
-    for(size_t i = 0; i < graph->numVertices; i++) {
+    for(size_t i = 0; i < graph->number_edges; i++) {
         if(scanf("%u %u", &x, &y) != 2) {
             puts("0");
             exit(EXIT_SUCCESS);
@@ -83,12 +160,12 @@ void build_inverted_graph(Graph* graph) {
 
 int main() {
     
-    unsigned int v1, v2, numVertices, numEdges;
+    unsigned int v1, v2, number_vertices, number_edges;
 
     scanf("%u %u", &v1, &v2);
-    scanf("%u %u", &numVertices, &numEdges);
+    scanf("%u %u", &number_vertices, &number_edges);
 
-    Graph* graph = new Graph(numVertices, numEdges);
+    Graph* graph = new Graph(number_vertices, number_edges);
     build_inverted_graph(graph);
 
     if(!graph->IsDAG()) {
@@ -96,6 +173,7 @@ int main() {
         return 0;
     }
 
+    graph->LCAs(v1, v2);
     
     return 0;
 }
